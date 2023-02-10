5f0b5b5551ee0ee79edf516d5be3b2ddc65403f1
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-10 14:40:16 +0530

    Nullability - billion dollar mistake

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,22 @@ void main(List<String> arguments) {
   String stringVariable = "Hello WhiteHorse!!";
   print(stringVariable);
 
-// We have been declaring variables but now we can do it wihout that too.
+  String nullVariable = null; // ERROR
+  nullVariable.length;
+  // THe problem with null is that the devloper doenst get the error rather the
+  // user gets all the errors at run time.
+  // NULL was a billion dollar mistake in programming.
 
-  var string = "Hello Saurabh";
-  // this will automatically take it as String. But this is not dynamic!!
-  string = "Value Changed!!";
-  // if you donot want to change the values then you can use FINAL instaed of var.
+  // Run time errors are bad and if you can get them at compile time which is
+  // what nullability accomplishes then its best.
+  // This is used in Dart.
 
-  final noChangeString = "This cannot be changed";
-  // noChangeString = "Second Value";// We will get error here
-  // Final Variables must be initialised on the same line.
-  // you cannot late initialize it.
+  String? nullVariable2 = null;
+  // ? this nullablity operator is a safety check whihc signifies that if the
+  // value is null then automatically skip that part rather than crashing the application
 
-  // Initializing vs non initialised variable.
-  String saurabh = "Something"; // this is initialized in same line.
-  String lateInitialize; //Type: String
+  nullVariable2?.length;
 
-  //ERRROR!!!!!!!!!!!!!!!!!!!!!!!!!
-  //print(lateInitialize)// Here we will get error
-  //The non-nullable local variable 'lateInitialize' must be assigned before it can be used.
-
-  lateInitialize =
-      "We will not get error for not initializing and datatype will remain string but in case of var";
-
-  var lateInitializeVar; //Type: dynamic
-  lateInitializeVar = "SKP";
-  // Here it will take dynamic as its data type.
+  String? nullVariable3;
+  // we donot need to explicitly assign as null beacuse null is the default value.
 }
