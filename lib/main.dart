556b03f3ebd515ea36b4d4eb0ec0f305e2772bd7
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-10 15:22:47 +0530

    Functions - Nested and Global.

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,24 @@ import 'package:flutter/material.dart';
 void main(List<String> arguments) {
   String stringVariable = "Hello WhiteHorse!!";
   print(stringVariable);
-// Control Flow Constructs
-// Basic building blocks of logic
-// Loops Classics.
-// Usually very less use of loops because mostly we write functions.
 
-  for (int i = 0; i < 10; i++) {
-    print("Value of i $i");
-  } // These loops are for n number of times.
-
-  // While loops are imp when you are not sure how many times the loops will move
-  bool condition = true;
-  while (condition) {
-    /// acc to condition change value to true
-    condition = false;
-  } // you are not even going to write many while loops.
-  /// usually we do it with functions only.
-
-  /// Another loop which we will never use is DO WHILE LOOP.
-
-  bool doLoop = false;
-  do {
-    print("Do done");
-    doLoop = true;
-  } while (false);
+  String returnStringNestedFunction() {
+    return "Hello";
+  }
+  // Nested Functions can only be called inside where declared.
+  // Functions are methods are basically the same thing.
+  // They provide the scope for codes.
+  // Scoping everything is very imp, in functions and also in classes.
 }
+
+/// We use very less loops and mostly use functions
+
+/// Above we already have a funciton - void main.
+/// Top Level Functions are which are not nested inside other functions.
+/// Strings returning Strings
+String returnString() {
+  return "Hello";
+}
+
+/// ERROR - The declaration 'returnString' isn't referenced.
+/// This means that the funciton is not getting used at any place.
