369363af8afbf75d22568d623802786d073a9ae7
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-10 14:32:34 +0530

    More on variables adn initializing them. Now we will talk about nullables.

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,30 @@ void main(List<String> arguments) {
   String stringVariable = "Hello WhiteHorse!!";
   print(stringVariable);
 
-  // Methods of String which are avalaible on datatype String.
-  print(stringVariable.contains("Hello"));
+// We have been declaring variables but now we can do it wihout that too.
 
-  // There are no long or float data types in dart.
-  int x = 5;
-  x.toString();
-  x.isEven;
-  // both int and double inherits num dataypes.
-  num hello = 4;
-  num hello2 = 4.4;
+  var string = "Hello Saurabh";
+  // this will automatically take it as String. But this is not dynamic!!
+  string = "Value Changed!!";
+  // if you donot want to change the values then you can use FINAL instaed of var.
 
-  dynamic worstToUse = "Something or anything can be here!!";
-  worstToUse = 5;
-  worstToUse = true;
-  // We will not get error at any place. Should not be used or least to use.
+  final noChangeString = "This cannot be changed";
+  // noChangeString = "Second Value";// We will get error here
+  // Final Variables must be initialised on the same line.
+  // you cannot late initialize it.
+
+  // Initializing vs non initialised variable.
+  String saurabh = "Something"; // this is initialized in same line.
+  String lateInitialize; //Type: String
+
+  //ERRROR!!!!!!!!!!!!!!!!!!!!!!!!!
+  //print(lateInitialize)// Here we will get error
+  //The non-nullable local variable 'lateInitialize' must be assigned before it can be used.
+
+  lateInitialize =
+      "We will not get error for not initializing and datatype will remain string but in case of var";
+
+  var lateInitializeVar; //Type: dynamic
+  lateInitializeVar = "SKP";
+  // Here it will take dynamic as its data type.
 }
