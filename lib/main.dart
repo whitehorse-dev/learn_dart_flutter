b099ceac12016fa372c14ba34c140188ddb68234
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-11 15:12:12 +0530

    Mixins

--- lib/main.dart
+++ lib/main.dart
@@ -3,90 +3,59 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> arguments) {
-  ImplementingClass() as RandomClass;
-}
-
-// In Kotlin, Java, typescript have INTERFACE
-// C and Objective C has protocols
-
-/// DART HAS IMPLICIT INTERFACES.
-/// we can implement any kind of class as an interface.
-/// Interface basically means properties and methods of the class without
-/// their implementation.
-///
-/// Nothing gets copied when we implement a class ie use interace.
-/// We have to give all definitons again newly.
-///
-/// We need to provide our own new implementation.
-///
-///
-
-class RandomClass {
-  String a;
-  String b;
-  RandomClass({
-    required this.a,
-    required this.b,
-  });
-
-  String get ab => '$a $b';
-
-  void printValue() {
-    print('Printed Value');
-  }
-}
-
-class ImplementingClass implements RandomClass {
-  // This will automatically copy all methods and properties and
-  // force you to write implementation again.
-  @override
-  String a;
-
-  @override
-  String b;
-
-  @override
-  // TODO: implement ab
-  String get ab => throw UnimplementedError();
-
-  @override
-  void printValue() {
-    // TODO: implement printValue
-  }
-}
-
-/// Care must be taken that UNLESS YOU HAVE A VERY GOOD REASON TO DO SO
-/// Like in test classes. you should only implement abstract classes and not regular
-/// classes.
-///
-///
-/// eg
-///
-abstract class DataReader<T> {
-  T ReadData();
-  // We are returning dynamic here because there cna be multiple data readers like
-  /// int data reader, StringDataReader etc...
+  // Mixins are similar to classes but to avoid multiple inheritance it can be used.
+  /// It uses with.... WITH
   ///
-  /// As Dynamic is not good to use we can use Generic Types.
+  ///
+  /// class x with ElevatedClient {}
+  ///
+  /// This is what Mixin does 3:16 minutes.
+  ///   You can think of Mixins as copyign a method adn pasting it
+  /// but it makes it pasting multiple times not required.
+  ///
+  /// It copies and pastes the members of mixins.
+  ///
+  var dog = Dog();
+  dog.breathe();
+  dog.bark();
+
+  var snake = Snake();
+  snake.display();
 }
 
-class IntegerDataReader implements DataReader<int> {
-  @override
-  // ALWAYS write return type as a good measure.
-  int ReadData() {
-    print('Yeah!! I am reading the population data');
-    return 12345;
+// Creating a Bark mixin
+mixin Bark {
+  void bark() => print('Barking');
+}
+
+mixin Fly {
+  void fly() => print('Flying');
+}
+
+mixin Crawl {
+  void crawl() => print('Crawling');
+}
+
+// Creating an Animal class
+class Animal {
+  void breathe() {
+    print("Breathing");
   }
 }
 
-class StringDataReader implements DataReader<String> {
-  @override
-  String ReadData() {
-    // TODO: implement ReadData
-    return 'String Returned';
+// Createing a Dog class, which extends the Animal class
+// Bark is the mixin with the method that Dog can implement
+class Dog extends Animal with Bark {}
+
+// Creating a Bat class Bat, which extends the Animal class
+// Fly is the mixin with the method that Bat can implement
+class Bat extends Animal with Fly {}
+
+class Snake extends Animal with Crawl {
+  // Invoking the methods within the display
+  void display() {
+    print(".....Snake.....");
+    breathe();
+    crawl();
   }
 }
-
-
-/// All these use the same class but has different generic data types. 
-/// DART DOESNT Support multiple INHERITANCE!!! GREAT THING!! 
