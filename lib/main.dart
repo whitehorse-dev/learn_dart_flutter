8eb1c5ced64d8a2d73782e984c39c20bf79c0cd8
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-11 16:48:29 +0530

    Packages and part vs part of

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
+part 'other_file.dart';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> arguments) {
-  final x = 'Saurabh'.duplicate();
-  print(x);
-
-  final y = "Nivedita".duplicated;
-  print(y);
+  /// In dart we only have package private access modifiers.
 }
 
-/// Eg String is a built in type and we cannot edit code for String.
-/// however we can press F12 to go to definition of String.
-///
-/// How to add a method to a string to make our lives easier??
-///
-/// Like we donot have a string.duplicate() method to duplicate a string.
-///
-/// EXTENSION.
-///
-extension StringDuplication on String {
-  String duplicate() {
-    return this + this;
-  }
 
-  // get methods donot need () rather directly braces. These are not methods/funcitons.
-  String get duplicated {
-    return this + this;
-  }
+/// This is package private class having private field and even pvt constrcutor
+class _PackagePrivateClass {
+  final String _packagePrivateField;
+  _PackagePrivateClass._(this._packagePrivateField);
 }
 
-/// We can also create properties. 
+// How to have a link bw files so that the classes are accessible in other classes. 
---
+++ lib/other_file.dart
@@ -0,0 +1,6 @@
+part of 'main.dart';
+// This makes the other file part of main.dart thats why we are able to use it.
+
+void Example() {
+  _PackagePrivateClass._("Private Package");
+}
