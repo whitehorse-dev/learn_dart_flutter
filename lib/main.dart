34a4f936cb05c67d58fa93d7a96c5e2810f87064
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-11 13:54:44 +0530

    One more imp topic in Classes - Equality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> arguments) {
-  User x = User(firstName: "Saurabh", lastName: "Pandey", email: 's@k.com');
-  x.fullName;
+  User x = const User(firstName: "Saurabh", lastName: "Pandey");
 
-  // first FullName is much better and lightweight.
-  x.fullName; // this is indistingusable from below one!! regular field.
-  x.lastName;
-  x._email =
-      "saurabh@gmail.com"; // Here we can access the private field but normally
-  // this is not the case because classes are in different file and usage is in other files.
+  User x2 = User(firstName: "Saurabh", lastName: "Pandey");
 
-// Using setter method
-  x.setEmail('saurabh');
+  User x3 = User(firstName: "Saurabh", lastName: "Pandey");
 
-  // Using set Property - better usage and this is how it should be done.
-  x.email = 'saurabh@gmail.com';
+  /// Here all the users x1, x2 and x3 are same. Refers to the same value.
+  ///
+  print(x2 == x3);
+  // Becuase they are instanciated with same cannonical instances.
+
+  // But when we change the value of one of the parameter then the value changes.
+  /// becuase I changed to first Name to Nivedita and now they are not the same anymore.
+  /// BoxedSize(width:8) will be same but width 9 will take up different space!!
+  ///
+  ///
+  /// But even with the same parameters if we conot use the const constructors then the
+  /// objects would not be true rather false.
+  ///
+  ///  User x2 = User(firstName: "Saurabh", lastName: "Pandey");
+
+  // User x3 = User(firstName: "Saurabh", lastName: "Pandey");
+  /// The above would be false even if everythign is same.
+  ///
+
+  /// It is all about referential equality.
+  /// Objects are not the same even though they hold the same data if constructors are
+  /// not const.
+  ///
 }
 
 class User {
   final String firstName;
   final String lastName;
-  String? _email;
 
-  User(
-      {required this.firstName,
-      required this.lastName,
-      required String email}) {
-    this._email =
-        email; // This will automatically check with the setting property
-  }
+  const User({
+    required this.firstName,
+    required this.lastName,
+  });
 
-  /// According to dart guidelines Methods should perform some actual work and the
-  /// above moethod is acutally not doing anything!!
-  ///
-  /// Properties are nice methods same as above but they are light weight.
-  ///
-  String get fullName =>
-      '$firstName $lastName'; // VS Code will show it as a property signified by wrench.
-  /// It will be saved like a property and not as a fully blown method.
-  ///
-  ///
-  void setEmail(String value) {
-    if (value.contains('@')) {
-      _email = value;
-    } else {
-      _email = null;
-    }
-  }
-
-  // Instead of above method we can also create a property like
-
-  set email(String value) {
-    if (value.contains('@')) {
-      _email = value;
-    } else {
-      _email = null;
-    }
-  }
-
-  String get email =>
-      _email ?? 'Not a valid email'; //?? this is coalesing operator
-  /// Work is to check if the previous value is null then automatically send next set error info.
-
-  /// we pass values into set properties
-  /// we get values out of get properties like get Fullname was string.
+  // Objects constructed with const constructors are always same.
 }
