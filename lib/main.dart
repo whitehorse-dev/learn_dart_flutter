a2746cdd1256babd242af6cdbaf131df55ae3e87
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-11 16:56:05 +0530

    Boring way to update immutable classes
    Using Data Classes or Struts

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
-part 'other_file.dart';
-
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> arguments) {
-  /// In dart we only have package private access modifiers.
+  final saurabh = Person(name: 'Saruabh', age: 30);
+  saurabh.age = 29;
+
+  /// In flutter is especailly useful to prevent updating the age and name of the person
+  /// So add final to the fields.
+  ///
+  /// Instead of modifying directly saurabh's age we now take a different person.
+  ///
+  final updatedSaurabh = Person(age: saurabh.age + 1, name: saurabh.name);
+
+  /// The updatedSaurabh is a copy of the earlier Saurabh with new age.
+  ///
+  /// MUTABLE - when you can directly assign the value then its mutable.
+  ///
+  /// IMMUTABLE - when you can't directly assign and go via other way.
+}
+
+@immutable // This means the class has only final fields and doesnt update values
+class Person {
+  final String name;
+  final int age;
+  const Person({
+    required this.name,
+    required this.age,
+  });
 }
 
 
-/// This is package private class having private field and even pvt constrcutor
-class _PackagePrivateClass {
-  final String _packagePrivateField;
-  _PackagePrivateClass._(this._packagePrivateField);
-}
-
-// How to have a link bw files so that the classes are accessible in other classes. 
+/// The above way is not a good way because its a long way and copies the person
+/// values and its long to write. 
--- lib/other_file.dart
+++ lib/other_file.dart
@@ -1,6 +1,1 @@
-part of 'main.dart';
-// This makes the other file part of main.dart thats why we are able to use it.
 
-void Example() {
-  _PackagePrivateClass._("Private Package");
-}
