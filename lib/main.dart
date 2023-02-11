71b95c56e6587ebacc79a78d09ee207c3520222c
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-11 17:03:10 +0530

    Generate Data Class
    Generated lot of helpful codes!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> arguments) {
   final saurabh = Person(name: 'Saruabh', age: 30);
-  saurabh.age = 29;
+  // saurabh.age = 29;
 
   /// In flutter is especailly useful to prevent updating the age and name of the person
   /// So add final to the fields.
@@ -30,9 +32,49 @@ class Person {
     required this.age,
   });
 
-  Person copyWith({String? name, int? age}) {
-    return Person(name: name ?? this.name, age: age ?? this.age);
+  Person copyWith({
+    String? name,
+    int? age,
+  }) {
+    return Person(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'age': age,
+    };
+  }
+
+  factory Person.fromMap(Map<String, dynamic> map) {
+    return Person(
+      name: map['name'] as String,
+      age: map['age'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Person.fromJson(String source) =>
+      Person.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Person(name: $name, age: $age)';
+
+// Now 2 objects of this are not only equal when there reference is equal but
+// also equal when their values are equal.
+  @override
+  bool operator ==(covariant Person other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name && other.age == age; // equal value check.
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ age.hashCode;
 }
 
 /// The above way is not a good way because its a long way and copies the person
