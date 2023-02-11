1bfa85eed2f8d5ec566fab50ea5522cce51dbdad
White Horse Dev <whitehorse.dev.sp@gmail.com>
2023-02-11 12:20:15 +0530

    Making email private and checking correct email type.

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,23 @@ void main(List<String> arguments) {
   // first FullName is much better and lightweight.
   x.fullName; // this is indistingusable from below one!! regular field.
   x.lastName;
+  x._email =
+      "saurabh@gmail.com"; // Here we can access the private field but normally
+  // this is not the case because classes are in different file and usage is in other files.
+
+// Using setter method
+  x.setEmail('saurabh');
+
+  // Using set Property - better usage and this is how it should be done.
+  x.emailSet = 'saurabh@gmail.com';
 }
 
 class User {
   final String firstName;
   final String lastName;
+  String? _email;
 
   User({required this.firstName, required this.lastName});
-
-  String getFullName() => '$firstName $lastName';
 
   /// According to dart guidelines Methods should perform some actual work and the
   /// above moethod is acutally not doing anything!!
@@ -27,4 +35,25 @@ class User {
       '$firstName $lastName'; // VS Code will show it as a property signified by wrench.
   /// It will be saved like a property and not as a fully blown method.
   ///
+  ///
+  void setEmail(String value) {
+    if (value.contains('@')) {
+      _email = value;
+    } else {
+      _email = null;
+    }
+  }
+
+  // Instead of above method we can also create a property like
+
+  set emailSet(String value) {
+    if (value.contains('@')) {
+      _email = value;
+    } else {
+      _email = null;
+    }
+  }
+
+  /// we pass values into set properties
+  /// we get values out of get properties like get Fullname was string.
 }
